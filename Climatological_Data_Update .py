07dd184e03f47ebc9ff7d69464db354575fbffdf
IandRover <0102030405jacky@gmail.com>
2016-09-17 22:12:30 +0800

    Update Climatological_Data_Update .py

--- Climatological_Data_Update .py
+++ Climatological_Data_Update .py
@@ -18,18 +18,11 @@ import random
 # In[2]:
 
 def read_station_data():
-    with open('.\\data\\地區資訊new.csv', 'r') as f: 
+    with open('.\\data\\Meteorological_stations_table.csv', 'r') as f: 
         reader = csv.reader(f)
         station_data = list(list(rec) for rec in csv.reader(f, delimiter=',')) #reads csv into a list of lists
         f.close()
     return station_data
-
-def read_pest_data(pest):
-    with open('.\\data\\蔬果重要害蟲防疫旬報_'+pest+'_new.csv','r') as f:
-        reader = csv.reader(f)
-        pest_data = list(list(rec) for rec in csv.reader(f, delimiter=','))
-        f.close()
-    return pest_data
 
 
 # In[3]:
